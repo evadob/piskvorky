ca8426a753c829211a41e61356d6be8c75190498
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-04-10 19:13:29 +0200

    added classes for player icons, added switching of the players icons

--- hra.js
+++ hra.js
@@ -1,5 +1,7 @@
 let currentPlayer = "circle";
 
+let playerIcon = document.querySelector(".player--circle");
+
 const square1 = document.querySelector("button:nth-child(1)");
 const square2 = document.querySelector("button:nth-child(2)");
 const square3 = document.querySelector("button:nth-child(3)");
@@ -17,10 +19,14 @@ const playedSquare = (event) => {
     let btnElm = document.querySelector(".square");
     event.target.classList.add("board__field--circle");
     currentPlayer = "cross";
+    playerIcon.classList.remove("player--circle");
+    playerIcon.classList.add("player--cross");
   } else {
     let btnElm = document.querySelector(".square");
     event.target.classList.add("board__field--cross");
     currentPlayer = "circle";
+    playerIcon.classList.remove("player--cross");
+    playerIcon.classList.add("player--circle");
   }
 };
 
