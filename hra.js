264f890e48195b5c117e3250f7786d335a06c953
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-04-20 17:15:33 +0200

    added querySelectorAll and forEach

--- hra.js
+++ hra.js
@@ -1,17 +1,10 @@
+import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
+
 let currentPlayer = "circle";
 
 let playerIcon = document.querySelector(".player--circle");
 
-const square1 = document.querySelector("button:nth-child(1)");
-const square2 = document.querySelector("button:nth-child(2)");
-const square3 = document.querySelector("button:nth-child(3)");
-const square4 = document.querySelector("button:nth-child(4)");
-const square5 = document.querySelector("button:nth-child(5)");
-const square6 = document.querySelector("button:nth-child(6)");
-const square7 = document.querySelector("button:nth-child(7)");
-const square8 = document.querySelector("button:nth-child(8)");
-const square9 = document.querySelector("button:nth-child(9)");
-const square10 = document.querySelector("button:nth-child(10)");
+const buttons = document.querySelectorAll("button");
 
 const playedSquare = (event) => {
   event.target.disabled = true;
@@ -30,13 +23,6 @@ const playedSquare = (event) => {
   }
 };
 
-square1.addEventListener("click", playedSquare);
-square2.addEventListener("click", playedSquare);
-square3.addEventListener("click", playedSquare);
-square4.addEventListener("click", playedSquare);
-square5.addEventListener("click", playedSquare);
-square6.addEventListener("click", playedSquare);
-square7.addEventListener("click", playedSquare);
-square8.addEventListener("click", playedSquare);
-square9.addEventListener("click", playedSquare);
-square10.addEventListener("click", playedSquare);
+buttons.forEach((button) => {
+  button.addEventListener("click", playedSquare);
+});
