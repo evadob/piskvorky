19b3109baf8e6719d8bbda3ae68f34cbda25484a
Eva Dobružská (evadob) <eva.dobruzska@gmail.com>
2023-04-20 19:57:16 +0200

    added findWinner and playAgain

--- hra.js
+++ hra.js
@@ -1,28 +1,56 @@
 import { findWinner } from "https://unpkg.com/piskvorky@0.1.4";
 
 let currentPlayer = "circle";
-
 let playerIcon = document.querySelector(".player--circle");
-
-const buttons = document.querySelectorAll("button");
 
 const playedSquare = (event) => {
   event.target.disabled = true;
   if (currentPlayer === "circle") {
-    let btnElm = document.querySelector(".square");
     event.target.classList.add("board__field--circle");
     currentPlayer = "cross";
     playerIcon.classList.remove("player--circle");
     playerIcon.classList.add("player--cross");
   } else {
-    let btnElm = document.querySelector(".square");
     event.target.classList.add("board__field--cross");
     currentPlayer = "circle";
     playerIcon.classList.remove("player--cross");
     playerIcon.classList.add("player--circle");
   }
+
+  const squares = document.querySelectorAll("button");
+  const squaresArray = Array.from(squares);
+  const playBoard = squaresArray.map((button) => {
+    if (button.classList.contains("board__field--circle")) {
+      return "o";
+    } else if (button.classList.contains("board__field--cross")) {
+      return "x";
+    } else {
+      return "_";
+    }
+  });
+
+  const winner = findWinner(playBoard);
+  if (winner === "x" || winner === "o") {
+    setTimeout(() => {
+      alert(`Vyhrál hráč se symbolem ${winner}`);
+      location.reload();
+    }, 222);
+  } else if (winner === "tie") {
+    setTimeout(() => {
+      alert("Pat a Mat!");
+      location.reload();
+    }, 222);
+  }
 };
 
-buttons.forEach((button) => {
+document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", playedSquare);
+});
+
+// play again
+const playAgain = document.querySelector(".button--blue");
+playAgain.addEventListener("click", (event) => {
+  if (!confirm("Nová hra? Určitě? Baví tě to?")) {
+    event.preventDefault();
+  }
 });
